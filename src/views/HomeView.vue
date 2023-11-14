7e84670a952101661162b4bd100b49cb3d4ee82f
ahmed-benkrara <ahmed.benkrara12@gmail.com>
2023-11-15 00:35:56 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <!-- <h1 class="text-red-600 text-[30px] font-poppins">Hello world</h1> -->
+  </main>
+</template>
+
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
